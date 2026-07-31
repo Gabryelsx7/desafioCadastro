2a1f3ff8e3cdefcc5904aca95b8c2b58ded323d9
Gabryelsx7 <gabryelchianca8@gmail.com>
2026-07-31 16:39:13 -0300

    feat(db): cria schema inicial do banco de dados

--- src/main/resources/db/migration/V1__create_pets_table.sql
+++ src/main/resources/db/migration/V1__create_pets_table.sql
@@ -1,19 +1,26 @@
-CREATE TABLE petShop
-(
-    id          UUID PRIMARY KEY DEFAULT gen_random_uuid(),
-    nome        varchar(120) NOT NULL,
-    sobrenome   varchar(120),
-    tipo        varchar(20)  NOT NULL,
-    sexo        varchar(20)  NOT NULL,
-    numero_casa varchar(20)      DEFAULT 'NÃO_INFORMADO',
-    cidade      varchar(120)     DEFAULT 'NÃO_INFORMADO',
-    rua         varchar(120)     DEFAULT 'NÃO_INFORMADO',
-    idade       NUMERIC(4, 2)    DEFAULT 0,
-    peso        NUMERIC(5, 2)    DEFAULT 0,
-    raca     varchar(120)     DEFAULT 'NÃO_INFORMADO',
-    ativo  BOOLEAN not null default true,
-    data_cadastro TIMESTAMP NOT NULL DEFAULT now()
+CREATE EXTENSION IF NOT EXISTS unaccent;
 
-)
-CREATE INDEX idx_petShop_nome ON pets (LOWER(unaccent(nome)));
-CREATE INDEX idx_petShop_tipo ON pets (tipo);
+CREATE OR REPLACE FUNCTION imutavel_unaccent(text)
+RETURNS text AS $$
+SELECT unaccent('unaccent', $1)
+           $$ LANGUAGE sql IMMUTABLE;
+
+CREATE TABLE pets
+(
+    id            UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    nome          varchar(120) NOT NULL,
+    sobrenome     varchar(120),
+    tipo          varchar(20)  NOT NULL,
+    sexo          varchar(20)  NOT NULL,
+    numero_casa   varchar(20)   DEFAULT 'NAO_INFORMADO',
+    cidade        varchar(120)  DEFAULT 'NAO_INFORMADO',
+    rua           varchar(120)  DEFAULT 'NAO_INFORMADO',
+    idade         NUMERIC(4, 2) DEFAULT 0,
+    peso          NUMERIC(5, 2) DEFAULT 0,
+    raca          varchar(120)  DEFAULT 'NAO_INFORMADO',
+    ativo         BOOLEAN NOT NULL DEFAULT true,
+    data_cadastro TIMESTAMP NOT NULL DEFAULT now()
+);
+
+CREATE INDEX idx_pets_nome ON pets (LOWER(imutavel_unaccent(nome)));
+CREATE INDEX idx_pets_tipo ON pets (tipo);
