e2bdf05f24a7b6dea7395f923f0d944a066b3c58
Gabryelsx7 <gabryelchianca8@gmail.com>
2026-08-01 15:57:34 -0300

    fix: corrige erros de sintaxe

--- src/test/java/com/example/desafioCadastro/service/PetServiceTest.java
+++ src/test/java/com/example/desafioCadastro/service/PetServiceTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
